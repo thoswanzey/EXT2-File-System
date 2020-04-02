c8a393905c3495a593494a35786f2c8aaf3bbb20
Thomas Swanzey <swanzem11@gmail.com>
2020-04-01 18:20:03 -0700

    Fixed bug where link_count wouldn't change, unlink is completed for now.

--- link.c
+++ link.c
@@ -44,7 +44,6 @@ int my_link(char *old_file, char *new_file)
     pmip = iget(dev, pino);
 
     enter_name(pmip, ino, child); // add hard link entry
-    printf("INODE: %d, links_count: %d\n", mip->ino, ip->i_links_count);
 
     ip->i_links_count++; // increment link count
     printf("INODE: %d, links_count: %d\n", mip->ino, ip->i_links_count);
@@ -86,32 +85,35 @@ int my_unlink(char *pathname)
     strcpy(buf, pathname);
     strcpy(child, basename(buf)); // basename destroys path
 
+    pino = getino(parent);
+    pmip = iget(dev, pino);
 
-    printf("INODE: %d, links_count: %d\n", mip->ino, ip->i_links_count);
+    rm_child(pmip, child); // remove entry from dir
+
+    pip->i_atime = pip->i_mtime = time(NULL);
+    pmip->dirty = 1;
+    iput(pmip);
+
 
     ip->i_links_count--;
     printf("INODE: %d, links_count: %d\n", mip->ino, ip->i_links_count);
-    if(ip->i_links_count == 0)
+
+    if(ip->i_links_count > 0)
     {
-        for (int i=0; i<12; i++){
+        mip->dirty = 1;
+    } 
+    else
+    {
+        for (int i=0; i<12; i++){ // delete inode
             if (ip->i_block[i]==0)
                 break;
             bdalloc(mip->dev, ip->i_block[i]);
         }
         idalloc(mip->dev, mip->ino);
     }
-
-    pino = getino(parent);
-    pmip = iget(dev, pino);
-
-    rm_child(pmip, child);
-
-    pip->i_atime = pip->i_mtime = time(NULL);
-    pmip->dirty = 1;
     
     
-    iput(pmip);
-    iput(mip); //(which clears mip->refCount = 0);
+    iput(mip); 
 
     return 0;    
 }
