b0a239c20c76e0598fbdb2e13e2bab7dc79274ad
John Opfer <opferjohn@gmail.com>
2020-04-15 21:36:18 -0700

    Fixed stupid mistakes in pfd causing seg faults

--- pfd.c
+++ pfd.c
@@ -8,7 +8,7 @@ int my_pfd(void)
     for( int i = 0; i < NFD; i++)
     {
         if(running->fd[i]){
-            printf("%-4d  %-10s  %-6d  [%-4d,%-4d]\n", i, modes[running->fd[i]->mode], running->fd[fd]->offset, running->fd[fd]->mptr->dev, running->fd[fd]->mptr->ino);
+            printf("%-4d  %-10s  %-6d  [%-4d,%-4d]\n", i, modes[running->fd[i]->mode], running->fd[i]->offset, running->fd[i]->mptr->dev, running->fd[i]->mptr->ino);
         }
     }
     return 0;
